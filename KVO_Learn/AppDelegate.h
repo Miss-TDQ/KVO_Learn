88df905e66b4f454c546c16159412b829235765d
taixiangwang <wtx21589@163.com>
2017-09-19 17:11:02 +0800

    Initial Commit

---
+++ KVO_Learn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVO_Learn
+//
+//  Created by ecaray_mac on 2017/9/19.
+//  Copyright © 2017年 charles_wtx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVO_Learn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVO_Learn
+//
+//  Created by ecaray_mac on 2017/9/19.
+//  Copyright © 2017年 charles_wtx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
